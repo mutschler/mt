b1325f0b67bfeef8533c46ada20e55bb11dbf310
mutschler <info@raphaelmutschler.de>
2016-07-07 10:12:59 +0200

    fix: include errors when upload fails

--- upload.go
+++ upload.go
@@ -29,14 +29,14 @@ func uploadFile(filename string) error {
     // open file handle
     fh, err := os.Open(filename)
     if err != nil {
-      log.Errorf("error opening file")
+      log.Errorf("error opening file: %s", err)
       return err
     }
 
     //iocopy
     _, err = io.Copy(fileWriter, fh)
     if err != nil {
-      log.Errorf("error iocopy")
+      log.Errorf("error iocopy: %s", err)
       return err
     }
 
@@ -45,13 +45,13 @@ func uploadFile(filename string) error {
 
     resp, err := http.Post(targetUrl, contentType, bodyBuf)
     if err != nil {
-      log.Errorf("error sending request to server")
+      log.Errorf("error sending request to server: %s", err)
       return err
     }
     defer resp.Body.Close()
     resp_body, err := ioutil.ReadAll(resp.Body)
     if err != nil {
-      log.Errorf("error reading server response")
+      log.Errorf("error reading server response: %s", err)
       return err
     }
     log.Infof("Server response:\n%s", string(resp_body))
