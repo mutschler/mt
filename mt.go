814acfcec03ececcc9bf0a3d518c8fa15c3f24dd
ploober <ploober@users.noreply.github.com>
2020-01-11 11:12:17 -0600

    resolves mutschler/mt#14 throw error when duration is less than interval

--- mt.go
+++ mt.go
@@ -121,7 +121,14 @@ func GenerateScreenshots(fn string) []image.Image {
 
 	numcaps = viper.GetInt("numcaps")
 	if viper.GetInt("interval") > 0 {
-		numcaps = int((duration / 1000) / int64(viper.GetInt("interval")))
+		var durationSec = duration / 1000
+		var intervalSec = int64(viper.GetInt("interval"))
+		if durationSec < intervalSec {
+			log.Fatalf("Specified interval is longer than video duration, " +
+				"use smaller interval or set numcaps instead.")
+			os.Exit(1)
+		}
+		numcaps = int(durationSec / intervalSec)
 		log.Debugf("interval option set, numcaps are set to %d", numcaps)
 		viper.Set("columns", int(math.Sqrt(float64(numcaps))))
 	}
