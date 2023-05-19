3f64371c6819202c7652ae07d33a08435f6439ef
Glottal Trill <philmslim@protonmail.com>
2023-05-19 16:46:55 -0400

    :white_check_mark: Expands on saveConfig test

--- config_test.go
+++ config_test.go
@@ -1,13 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"testing"
 )
 
 func TestSaveConfig(t *testing.T) {
-	currentDirectory, _ := os.Getwd()
-	got := saveConfig(currentDirectory)
+	testFile, _ := os.CreateTemp("", "saveTest.json")
+	defer func(name string) {
+		err := os.Remove(name)
+		if err != nil {
+			fmt.Println("could not delete saveTest.json")
+		}
+	}(testFile.Name())
+
+	got := saveConfig(testFile.Name())
 	if got != nil {
 		t.Errorf("got %q, wanted nil", got)
 	}
